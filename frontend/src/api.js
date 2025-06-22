fa8e8e80cc747bd4d0f7e5d386421983756bd16d
Rohit prasad <72071869+blank7k@users.noreply.github.com>
2025-06-22 21:42:19 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_BASE_URL = "https://manhwa-companion.onrender.com";
+
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: API_BASE_URL,
 });
 
 export const getRecommendations = async (moods) => {
